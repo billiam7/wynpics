2eaf2944b4429bc914d6f8948036c93f7a647c5d
Bill Lapko <wlapko@gmail.com>
2017-02-22 19:33:32 -0500

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :pictures
   resources :users do
-    resources :pictures 
+  resources :pictures
+
   end
+  root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/login' => 'sessions#new'
   get '/signup' => 'users#new'
